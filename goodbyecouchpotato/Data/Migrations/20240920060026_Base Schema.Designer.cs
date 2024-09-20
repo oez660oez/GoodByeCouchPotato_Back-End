bdf96105b5c341ea710001cd75f54c35c7883672
LiYuan <oez600oez@gmail.com>
2024-09-20 14:07:17 +0800

    會員登入新增"Base Schema"

--- goodbyecouchpotato/Data/Migrations/20240920060026_Base Schema.Designer.cs
+++ goodbyecouchpotato/Data/Migrations/20240920060026_Base Schema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using goodbyecouchpotato.Data;
 
@@ -11,9 +12,11 @@ using goodbyecouchpotato.Data;
 namespace goodbyecouchpotato.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240920060026_Base Schema")]
+    partial class BaseSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
