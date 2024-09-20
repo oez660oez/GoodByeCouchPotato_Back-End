ecf682feb3376dbe4647a3bab13241130db0f469
LiYuan <oez600oez@gmail.com>
2024-09-20 12:10:07 +0800

    會員登入介面、身分驗證建置

---
+++ goodbyecouchpotato/Areas/Identity/Controller/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Areas.MemberManagement.Controllers
+{
+	[Area("Identity")]
+	public class AccountController : Controller
+	{
+		// GET: /Account/Login
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		// POST: /Account/Login
+		[HttpPost]
+		public IActionResult Login(LoginViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				// 驗證邏輯 (例如檢查用戶名和密碼)
+				// 成功則登入用戶，失敗則回傳錯誤訊息
+				return RedirectToAction("Index", "Home");
+			}
+
+			return View(model);
+		}
+	}
+}
--- goodbyecouchpotato/Controllers/HomeController.cs
+++ goodbyecouchpotato/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using goodbyecouchpotato.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace goodbyecouchpotato.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
