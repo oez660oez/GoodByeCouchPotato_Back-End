31d64148c87997fa61be24e58a108c4c5297e574
莉莉安 Lillian <uo12354@gmail.com>
2024-09-21 17:15:14 +0800

    註冊管理員帳號

--- goodbyecouchpotato/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ goodbyecouchpotato/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -58,7 +58,7 @@ namespace goodbyecouchpotato.Areas.Identity.Pages.Account
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public class InputModel
+        public class InputModel   //viewmodel
         {
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
--- goodbyecouchpotato/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ goodbyecouchpotato/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -16,11 +16,14 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace goodbyecouchpotato.Areas.Identity.Pages.Account
 {
+    [Authorize(Roles = "Admin")]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
@@ -29,14 +32,17 @@ namespace goodbyecouchpotato.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;  //加入role di
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,  //加入role di
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender)
         {
+            _roleManager = roleManager;  //取得role
             _userManager = userManager;
             _userStore = userStore;
             _emailStore = GetEmailStore();
@@ -44,6 +50,8 @@ namespace goodbyecouchpotato.Areas.Identity.Pages.Account
             _logger = logger;
             _emailSender = emailSender;
         }
+
+
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -74,19 +82,19 @@ namespace goodbyecouchpotato.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Required(ErrorMessage = "信箱必須填寫")]
             [EmailAddress]
-            [Display(Name = "Email")]
+            [Display(Name = "電子信箱")]
             public string Email { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage ="密碼必須填寫")]
+            [StringLength(100, ErrorMessage = "{0}要至少為{2}到{1}個字", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "密碼")]
             public string Password { get; set; }
 
             /// <summary>
@@ -94,16 +102,28 @@ namespace goodbyecouchpotato.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "確認密碼")]
+            [Compare("Password", ErrorMessage = "確認密碼與密碼不一致！")]
             public string ConfirmPassword { get; set; }
-        }
 
+            [Required(ErrorMessage = "請設定該帳號的權限")]
+            [Display(Name = "設定權限")]
+            public string Role { get; set; } // 添加角色字段
+
+        }
+        public List<SelectListItem> RoleList { get; set; }
 
         public async Task OnGetAsync(string returnUrl = null)
         {
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            var roles = await _roleManager.Roles.ToListAsync();
+            RoleList = new List<SelectListItem>
+        {
+        new SelectListItem { Value = string.Empty, Text = "", Selected = true } //加上空白字段，令使用者必須選擇
+         };
+            RoleList.AddRange(roles.Select(r => new SelectListItem { Value = r.Name, Text = r.Name }));
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
@@ -112,12 +132,20 @@ namespace goodbyecouchpotato.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = CreateUser();
-
+                var user = CreateUser();  //調用方法來創建使用者，會將資料封裝在裡面，如果錯誤會整包退回不會建一半
+                                //將用戶信箱設置為用戶名字
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                                //將用戶信箱設置為信箱
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
+
+                //----------取得角色---------------
+                if (!string.IsNullOrEmpty(Input.Role))
+                {
+                    await _userManager.AddToRoleAsync(user, Input.Role);  //直接執行角色的分配
+                }
+                //----------取得角色---------------
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
