353bf7f4a90eaecd1e270ae3e12fa86cd8216f56
Leo <leo@gmail.com>
2024-09-21 17:14:57 +0800

    管理員管理--編輯

---
+++ goodbyecouchpotato/Areas/AdministratorManagement/Views/AdminViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace goodbyecouchpotato.Areas.AdministratorManagement.Views
+{
+    public partial class AdminViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public List<RoleInfo> Roles { get; set; }
+
+        public class RoleInfo
+        {
+            public string RoleId { get; set; }
+            public string RoleName { get; set; }
+        }
+    }
+}
