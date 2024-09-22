0a7427b481616b8b77d120d3b313c867a76df41f
Leo <leo@gmail.com>
2024-09-22 13:07:47 +0800

    權限編輯

--- goodbyecouchpotato/Areas/AdministratorManagement/Controllers/AdministratorsController.cs
+++ goodbyecouchpotato/Areas/AdministratorManagement/Controllers/AdministratorsController.cs
@@ -50,6 +50,7 @@ namespace goodbyecouchpotato.Areas.AdministratorManagement.Controllers
                     UserId = user.Id,
                     UserName = user.UserName,
                     Email = user.Email,
+
                     Roles = roles.Select(role =>
                     {
                         var matchedRole = allRoles.FirstOrDefault(r => r.Name == role);
@@ -67,143 +68,180 @@ namespace goodbyecouchpotato.Areas.AdministratorManagement.Controllers
             // 將 ViewModel 傳遞給視圖
             return View(adminViewModels);
         }
-    }
-}
 
-
-        // GET: AdministratorManagement/Administrators
-        //public async Task<IActionResult> Index()
-        //{
-        //    return View(await _context.Administrators.ToListAsync());
-        //}
-
-        // GET: AdministratorManagement/Administrators/Details/5
-        //public async Task<IActionResult> Details(string id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var administrator = await _context.Administrators
-        //        .FirstOrDefaultAsync(m => m.AAccount == id);
-        //    if (administrator == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(administrator);
-        //}
-
-        // GET: AdministratorManagement/Administrators/Create
-        //public IActionResult Create()
+        //public async Task<IActionResult> Edit()
         //{
         //    return View();
         //}
 
-        // POST: AdministratorManagement/Administrators/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("AAccount,APassword,MDailyTask,MProduct,MAdministrator")] Administrator administrator)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(administrator);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(administrator);
-        //}
 
-        // GET: AdministratorManagement/Administrators/Edit/5
-        //public async Task<IActionResult> Edit(string id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
 
-        //    var administrator = await _context.Administrators.FindAsync(id);
-        //    if (administrator == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(administrator);
-        //}
+        [HttpPost]
+        public async Task<IActionResult> ChangeUserRole(string userId, string oldRoleName, string newRoleName)
+        {
 
-        // POST: AdministratorManagement/Administrators/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(string id, [Bind("AAccount,APassword,MDailyTask,MProduct,MAdministrator")] Administrator administrator)
-        //{
-        //    if (id != administrator.AAccount)
-        //    {
-        //        return NotFound();
-        //    }
+            // 獲取當前用戶 ID
+            var currentUserId = _userManager.GetUserId(User);
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(administrator);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!AdministratorExists(administrator.AAccount))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(administrator);
-        //}
+            // 檢查是否是自己的帳號
+            if (userId == currentUserId)
+            {
+                ModelState.AddModelError("", "您無法修改自己的角色權限。");
+                return RedirectToAction("Index", "Administrators", new { area = "AdministratorManagement" });
+            }
 
-        // GET: AdministratorManagement/Administrators/Delete/5
-        //public async Task<IActionResult> Delete(string id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "找不到用戶。");
+                return RedirectToAction("Index", "Administrators", new { area = "AdministratorManagement" });
+            }
 
-        //    var administrator = await _context.Administrators
-        //        .FirstOrDefaultAsync(m => m.AAccount == id);
-        //    if (administrator == null)
-        //    {
-        //        return NotFound();
-        //    }
+            // 移除舊角色
+            if (!string.IsNullOrEmpty(oldRoleName))
+            {
+                var removeResult = await _userManager.RemoveFromRoleAsync(user, oldRoleName);
+                if (!removeResult.Succeeded)
+                {
+                    ModelState.AddModelError("", "移除舊角色失敗：" + string.Join(", ", removeResult.Errors.Select(e => e.Description)));
+                    return RedirectToAction("Index", "Administrators", new { area = "AdministratorManagement" });
+                }
+            }
 
-        //    return View(administrator);
-        //}
+            // 添加新角色
+            var addResult = await _userManager.AddToRoleAsync(user, newRoleName);
+            if (!addResult.Succeeded)
+            {
+                ModelState.AddModelError("", "分配新角色失敗：" + string.Join(", ", addResult.Errors.Select(e => e.Description)));
+                return RedirectToAction("Index", "Administrators", new { area = "AdministratorManagement" });
+            }
 
-        // POST: AdministratorManagement/Administrators/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-    //    public async Task<IActionResult> DeleteConfirmed(string id)
-    //    {
-    //        var administrator = await _context.Administrators.FindAsync(id);
-    //        if (administrator != null)
-    //        {
-    //            _context.Administrators.Remove(administrator);
-    //        }
+            // 成功更改角色，重定向回管理員列表頁面
+            return RedirectToAction("Index", "Administrators", new { area = "AdministratorManagement" });
+        }
+    }
+}
 
-    //        await _context.SaveChangesAsync();
-    //        return RedirectToAction(nameof(Index));
-    //    }
 
-    //    private bool AdministratorExists(string id)
-    //    {
-    //        return _context.Administrators.Any(e => e.AAccount == id);
-    //    }
-    //}
+// GET: AdministratorManagement/Administrators
+//public async Task<IActionResult> Index()
+//{
+//    return View(await _context.Administrators.ToListAsync());
+//}
+
+// GET: AdministratorManagement/Administrators/Details/5
+//public async Task<IActionResult> Details(string id)
+//{
+//    if (id == null)
+//    {
+//        return NotFound();
+//    }
+
+//    var administrator = await _context.Administrators
+//        .FirstOrDefaultAsync(m => m.AAccount == id);
+//    if (administrator == null)
+//    {
+//        return NotFound();
+//    }
+
+//    return View(administrator);
+//}
+
+// GET: AdministratorManagement/Administrators/Create
+//public IActionResult Create()
+//{
+//    return View();
+//}
+
+// POST: AdministratorManagement/Administrators/Create
+// To protect from overposting attacks, enable the specific properties you want to bind to.
+// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+//[HttpPost]
+//[ValidateAntiForgeryToken]
+//public async Task<IActionResult> Create([Bind("AAccount,APassword,MDailyTask,MProduct,MAdministrator")] Administrator administrator)
+//{
+//    if (ModelState.IsValid)
+//    {
+//        _context.Add(administrator);
+//        await _context.SaveChangesAsync();
+//        return RedirectToAction(nameof(Index));
+//    }
+//    return View(administrator);
+//}
+
+// GET: AdministratorManagement/Administrators/Edit/5
+
+// POST: AdministratorManagement/Administrators/Edit/5
+// To protect from overposting attacks, enable the specific properties you want to bind to.
+// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+//[HttpPost]
+//[ValidateAntiForgeryToken]
+//public async Task<IActionResult> Edit(string id, [Bind("AAccount,APassword,MDailyTask,MProduct,MAdministrator")] Administrator administrator)
+//{
+//    if (id != administrator.AAccount)
+//    {
+//        return NotFound();
+//    }
+
+//    if (ModelState.IsValid)
+//    {
+//        try
+//        {
+//            _context.Update(administrator);
+//            await _context.SaveChangesAsync();
+//        }
+//        catch (DbUpdateConcurrencyException)
+//        {
+//            if (!AdministratorExists(administrator.AAccount))
+//            {
+//                return NotFound();
+//            }
+//            else
+//            {
+//                throw;
+//            }
+//        }
+//        return RedirectToAction(nameof(Index));
+//    }
+//    return View(administrator);
+//}
+
+// GET: AdministratorManagement/Administrators/Delete/5
+//public async Task<IActionResult> Delete(string id)
+//{
+//    if (id == null)
+//    {
+//        return NotFound();
+//    }
+
+//    var administrator = await _context.Administrators
+//        .FirstOrDefaultAsync(m => m.AAccount == id);
+//    if (administrator == null)
+//    {
+//        return NotFound();
+//    }
+
+//    return View(administrator);
+//}
+
+// POST: AdministratorManagement/Administrators/Delete/5
+//[HttpPost, ActionName("Delete")]
+//[ValidateAntiForgeryToken]
+//    public async Task<IActionResult> DeleteConfirmed(string id)
+//    {
+//        var administrator = await _context.Administrators.FindAsync(id);
+//        if (administrator != null)
+//        {
+//            _context.Administrators.Remove(administrator);
+//        }
+
+//        await _context.SaveChangesAsync();
+//        return RedirectToAction(nameof(Index));
+//    }
+
+//    private bool AdministratorExists(string id)
+//    {
+//        return _context.Administrators.Any(e => e.AAccount == id);
+//    }
+//}
 //}
