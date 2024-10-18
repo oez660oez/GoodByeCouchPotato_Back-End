1c9bd3fcf9c298762203da44cfa501191d15e875
chun <chun@DESKTOP-B76AVGS>
2024-10-18 13:35:52 +0800

    完成前台聯絡我們+修改後台聯絡我們的回信格式

---
+++ PotatoWebAPI/DTO/FeedbackDTO.cs
@@ -0,0 +1,12 @@
+﻿
+namespace PotatoWebAPI.DTO
+{
+    public class FeedbackDTO
+    {
+        public string Email { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
+    }
+}
+
