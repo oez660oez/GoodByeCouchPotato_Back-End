82ce04084bd5a32d5eb060bac8d2da5914bbe339
chun <chun@DESKTOP-B76AVGS>
2024-10-22 13:33:08 +0800

    每日任務獲得

--- PotatoWebAPI/DTO/DailytaskDTO.cs
+++ PotatoWebAPI/DTO/DailytaskDTO.cs
@@ -1,13 +1,20 @@
 ﻿namespace PotatoWebAPI.DTO
 {
+    public class DailytaskgetidDTO
+    {
+        public int CId { get; set; }
+    }
     public class DailytaskDTO
     {
         public int CId { get; set; }
-        public string? T1name { get; set; }
-        public bool? T1completed { get; set; }
-        public string? T2name { get; set; }
-        public bool? T2completed { get; set; }
-        public string? T3name { get; set; }
-        public bool? T3completed { get; set; }
+        public string T1name { get; set; }
+        public bool T1completed { get; set; }
+        public int T1Reward { get; set; }
+        public string T2name { get; set; }
+        public bool T2completed { get; set; }
+        public int T2Reward { get; set; }
+        public string T3name { get; set; }
+        public bool T3completed { get; set; }
+        public int T3Reward { get; set; }
     }
 }
